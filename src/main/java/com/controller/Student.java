5b65a680da209eee4a92d62190845f253ced68ba
rakeshkamboj26 <rakeshkamboj26@gmail.com>
2017-11-16 22:01:37 +0530

    @JSON annotation
    Signed-off-by: rakeshkamboj26 <rakeshkamboj26@gmail.com>

--- src/main/java/com/controller/Student.java
+++ src/main/java/com/controller/Student.java
@@ -2,7 +2,16 @@ package com.controller;
 
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties("city")
+@JsonPropertyOrder({"full_name","address"})
 public class Student { 
+	
 	Address address;
 	public Address getAddress() {
 		return address;
@@ -10,8 +19,11 @@ public class Student {
 	public void setAddress(Address address) {
 		this.address = address;
 	}
+	
 	@Size(min=2,max=20)
+	@JsonProperty("full_Name")
 	String name;
+	
 	String city;
 	public String getName() {
 		return name;
